6646cd9980bf82dec12c9eeb6a60f04c4820506d
Daniel <you@example.com>
2021-06-27 12:09:23 +0700

    menambah postgre

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const NotesValidator = require('./validator/notes');
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.host,
     routes: {
       cors: {
         origin: ['*'],
